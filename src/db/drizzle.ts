b7491631d9190683ae6912d57829c7f5929c85e3
YGN <ygntechstartup@gmail.com>
2024-06-16 21:55:52 +0530

    Changed the var

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -3,5 +3,5 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 config({path:'.env'})
-export const sql = neon("postgresql://neondb_owner:cdE9yRws1Onu@ep-summer-king-a5s14l2z.us-east-2.aws.neon.tech/neondb?sslmode=require")
+export const sql = neon(process.env.DATABASE_URL!)
 export const db = drizzle(sql,{schema})
